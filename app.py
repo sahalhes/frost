6e6ef06a8744fb973ef41a3f6f98f629aae7548a
Sahal Hussain <sahalhes@gmail.com>
2023-12-06 14:06:32 +0530

    host

--- app.py
+++ app.py
@@ -401,4 +401,5 @@ def index():
     return render_template('index.html', stock_predictor=stock_predictor)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
